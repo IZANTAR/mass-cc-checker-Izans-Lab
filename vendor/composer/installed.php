45625f8a734a8b4e02c8495d1531309900be887c
Izan Tar <izantarteso@gmail.com>
2025-06-05 22:24:51 +0200

    Proyecto listo con configuración de entorno limpia

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e49359e9d442f5ae4792402deacb79e5ec2ea0f9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e49359e9d442f5ae4792402deacb79e5ec2ea0f9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'braintree/braintree_php' => array(
+            'pretty_version' => '6.26.0',
+            'version' => '6.26.0.0',
+            'reference' => '602bfe7876cc2f6c6c24bd68fef4c6de8c3a8195',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../braintree/braintree_php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
